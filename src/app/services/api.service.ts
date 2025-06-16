6ffac6ef13433744ad292f1048037ea105846ac2
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-16 20:42:42 +0000

    Implemented logic for getting data from the API and added error handling as well

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError, catchError } from 'rxjs';
+import { Posts } from '../model/posts.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Posts[]> {
+    return this.http
+      .get<Posts[]>(`${this.baseUrl}/posts`)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  getPost(id: number): Observable<Posts> {
+    return this.http
+      .get<Posts>(`${this.baseUrl}/posts/${id}`)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
