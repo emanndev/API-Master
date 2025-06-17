26ee7931105f23087e0fddd55dc164329d81598b
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-17 15:20:21 +0000

    Implemented Central error handling logic

--- src/app/services/error-handling.service.ts
+++ src/app/services/error-handling.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ErrorHandlingService {
-
-  constructor() { }
+  handleError(error: any): Observable<never> {
+    let errorMessage = 'An unknown error occurred';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${
+        error.message || 'Server error'
+      }`;
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
+  retryRequest(observable: any, retries: number = 1): any {
+    return observable.pipe(retry(retries));
+  }
+  constructor() {}
 }
