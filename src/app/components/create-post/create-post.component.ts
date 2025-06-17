4c9bf95c76283dc424cd0e19d5b7f773654a98cd
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-17 16:58:41 +0000

    Added logic for the create post component

--- src/app/components/create-post/create-post.component.ts
+++ src/app/components/create-post/create-post.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { Posts } from '../../model/posts.model';
 
 @Component({
   selector: 'app-create-post',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './create-post.component.html',
-  styleUrl: './create-post.component.scss'
+  styleUrls: ['./create-post.component.scss'],
 })
 export class CreatePostComponent {
+  newPost: Partial<Posts> = { title: '', body: '' };
 
+  constructor(private apiService: ApiService) {}
+
+  onSubmit() {
+    if (this.newPost.title && this.newPost.body) {
+      this.apiService.createPost(this.newPost).subscribe({
+        next: () => {
+          this.newPost = { title: '', body: '' };
+          window.alert('Post created successfully!');
+        },
+        error: (err) => console.error('Failed to create post:', err),
+      });
+    }
+  }
 }
