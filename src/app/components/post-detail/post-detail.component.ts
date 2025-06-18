bcb1090388a0c9f7db9c8fddcf5b0f9c34d9155f
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-18 16:24:28 +0000

    Changed template form in the post detail which handles the edit post to a reactive form to implement validaors

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -3,29 +3,61 @@ import { ApiService } from '../../services/api.service';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Posts, Comments } from '../../model/posts.model';
-import { FormsModule } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormArray,
+} from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { SanitizerService } from '../../services/sanitizer.service';
+import { restrictedContentValidator } from '../../validator/validators';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.scss'],
 })
 export class PostDetailComponent implements OnInit {
   post!: Posts;
   comments: Comments[] = [];
+  editForm: FormGroup;
   showEditModal = false;
-  editedPost: Partial<Posts> = {};
-  editedComments: Partial<Comments>[] = [];
+  imagePreview: SafeUrl = '';
 
   constructor(
     private apiService: ApiService,
     private route: ActivatedRoute,
     private router: Router,
-    public authService: AuthService
-  ) {}
+    private fb: FormBuilder,
+    public authService: AuthService,
+    private sanitizerService: SanitizerService
+  ) {
+    this.editForm = this.fb.group({
+      title: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          restrictedContentValidator(),
+        ],
+      ],
+      body: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(20),
+          restrictedContentValidator(),
+        ],
+      ],
+      imageUrl: [''],
+      comments: this.fb.array([]),
+    });
+  }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id')!;
@@ -36,10 +68,29 @@ export class PostDetailComponent implements OnInit {
     this.apiService.getComments(id).subscribe({
       next: (data) => {
         this.comments = data;
-        this.editedComments = data.map((comment) => ({ ...comment }));
+        this.initializeCommentsFormArray(data);
       },
       error: (err) => console.error('Failed to load comments:', err),
     });
+  }
+
+  get commentsFormArray(): FormArray {
+    return this.editForm.get('comments') as FormArray;
+  }
+
+  initializeCommentsFormArray(comments: Comments[]) {
+    const formArray = this.fb.array(
+      comments.map((comment) =>
+        this.fb.group({
+          id: [comment.id],
+          body: [
+            comment.body,
+            [Validators.required, restrictedContentValidator()],
+          ],
+        })
+      )
+    );
+    this.editForm.setControl('comments', formArray);
   }
 
   openEditModal() {
@@ -47,22 +98,41 @@ export class PostDetailComponent implements OnInit {
       this.promptLogin();
       return;
     }
-    this.editedPost = { ...this.post };
-    this.editedComments = this.comments.map((comment) => ({ ...comment }));
+    this.editForm.patchValue({
+      title: this.post.title,
+      body: this.post.body,
+      imageUrl: this.post.imageUrl || '',
+    });
+    this.initializeCommentsFormArray(this.comments);
+    this.imagePreview = this.sanitizerService.sanitizeUrl(
+      this.post.imageUrl || ''
+    );
+    this.editForm.get('imageUrl')?.valueChanges.subscribe((url) => {
+      this.imagePreview = this.sanitizerService.sanitizeUrl(url || '');
+    });
     this.showEditModal = true;
   }
 
   saveEdit() {
-    if (this.editedPost.title && this.editedPost.body) {
-      this.apiService.updatePosts(this.editedPost).subscribe({
-        next: (updatedPost) => {
-          this.post = updatedPost;
-          this.comments = this.editedComments as Comments[];
+    if (this.editForm.valid) {
+      const updatedPost = {
+        id: this.post.id,
+        ...this.editForm.value,
+      };
+      this.apiService.updatePosts(updatedPost).subscribe({
+        next: (updated) => {
+          this.post = updated;
+          this.comments = this.editForm.value.comments;
           this.showEditModal = false;
           window.alert('Post and comments updated successfully!');
         },
-        error: (err) => console.error('Failed to update post:', err),
+        error: (err) => {
+          console.error('Failed to update post:', err);
+          window.alert('Failed to update post. Please try again.');
+        },
       });
+    } else {
+      window.alert('Please correct the errors in the form.');
     }
   }
 
