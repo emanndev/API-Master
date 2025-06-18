1d4b52a1b920c28a4ac2517d363d537beb5c519d
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-18 14:43:13 +0000

    Implemented logic to handle onLogin method

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        if (response) {
+          this.router.navigate(['/']);
+        } else {
+          this.error = 'Invalid credentials';
+        }
+      },
+      error: (err) => (this.error = 'Login failed'),
+    });
+  }
 }
