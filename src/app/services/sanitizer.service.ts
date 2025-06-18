bfd4cc5a89e97483be7ef46960d2428a2d7f9ffa
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-18 16:19:05 +0000

    Implemented logic to prevent XSS in sanitizer service

--- src/app/services/sanitizer.service.ts
+++ src/app/services/sanitizer.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SanitizerService {
+  constructor(private sanitizer: DomSanitizer) {}
 
-  constructor() { }
+  sanitizeUrl(url: string): SafeUrl {
+    // Basic check to prevent javascript: URLs from being loaded (security risk - XSS)
+    if (url && !url.match(/^(https?:\/\/|data:image\/)/)) {
+      return this.sanitizer.bypassSecurityTrustUrl(''); // Return empty safe URL
+    }
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
 }
