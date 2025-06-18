8288fcd932754445e7f7d9d26eef50f2fbbe9e6c
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-18 14:45:43 +0000

    Implemnted login page and added styling to it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { CreatePostComponent } from './components/create-post/create-post.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,12 @@ export const routes: Routes = [
     path: 'post/:id',
     component: PostDetailComponent,
   },
-  { path: 'create-post', component: CreatePostComponent },
+  {
+    path: 'create-post',
+    component: CreatePostComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
